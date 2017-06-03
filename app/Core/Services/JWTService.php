0c9b6af9edd62daa542691abcc3835714a19a381
Andrey Degtyaruk <hlogeon1@gmail.com>
2017-06-03 21:56:32 +0700

    .env change

---
+++ app/Core/Services/JWTService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hlogeon
+ * Date: 03/06/2017
+ * Time: 21:49
+ */
+
+namespace App\Core\Services;
+
+
+class JWTService
+{
+
+}
---
+++ config/jwt.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hlogeon
+ * Date: 03/06/2017
+ * Time: 21:51
+ */
+
+return [
+    'key' => env('JWT_KEY'),
+];
